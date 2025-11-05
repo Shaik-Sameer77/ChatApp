13747f9836842ca074d43cc97edfc75b004c6a70
Shaik Sameer <130442189+Shaik-Sameer77@users.noreply.github.com>
2025-11-05 20:24:05 +0530

    added port to 465

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
+  port: 465,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
