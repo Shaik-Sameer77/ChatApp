20b063e5eba4bf3ed9da4f32fe98a3f4d59e8b2b
Shaik Sameer <130442189+Shaik-Sameer77@users.noreply.github.com>
2025-11-05 20:20:50 +0530

    nodemailer issue fixed

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -5,7 +5,6 @@ dotenv.config()
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
